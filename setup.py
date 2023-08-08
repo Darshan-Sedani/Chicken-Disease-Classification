adbe1a413354756c4dd7132555cd6f4b452d3621
Darshan Sedani <darshansedani81@gmail.com>
2023-08-08 16:50:06 +0530

    updating changes

--- setup.py
+++ setup.py
@@ -7,16 +7,14 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Chicken-Disease-Classification--Project"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "Darshan Sedani"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
 
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
-    author_email=AUTHOR_EMAIL,
     description="A small python package for CNN app",
     long_description=long_description,
     long_description_content="text/markdown",
